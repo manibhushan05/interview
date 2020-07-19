4929e503cd5f79416b9d8e52b039e3db36ad0164
Mani Kumar <manikumar@ManiBhushan.local>
2020-07-20 02:45:14 +0530

    jpa

--- system-design/spring-boot/petonix/src/main/java/in/pets/petonix/controller/AddressController.java
+++ system-design/spring-boot/petonix/src/main/java/in/pets/petonix/controller/AddressController.java
@@ -1,32 +1,29 @@
-//package in.pets.petonix.controller;
-//
-//import in.pets.petonix.model.ums.Address;
-//import in.pets.petonix.repository.AddressRepository;
-//import in.pets.petonix.service.AddressService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping(value = "/")
-//public class AddressController extends BaseController {
-//    @Autowired
-//    AddressRepository addressRepository;
-//
+package in.pets.petonix.controller;
+
+import in.pets.petonix.model.ums.Address;
+import in.pets.petonix.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class AddressController extends BaseController {
+    @Autowired
+    AddressRepository addressRepository;
+
 //    @Autowired
 //    AddressService addressService;
-//
-//    @PostMapping("address")
-//    public ResponseEntity<Address> createAdress(@RequestBody Address address) {
-//        Address addressData = addressRepository.save(address);
-//        return new ResponseEntity<>(addressData, HttpStatus.CREATED);
-//    }
+
+    @PostMapping("address")
+    public ResponseEntity<Address> createAdress(@RequestBody Address address) {
+        Address addressData = addressRepository.save(address);
+        return new ResponseEntity<>(addressData, HttpStatus.CREATED);
+    }
 //    @GetMapping(value = "address")
 //    public ResponseEntity<Map<String, Object>>  getAddress(@RequestParam Map<String, Object> requestParams,
 //                                                           HttpServletRequest request) {
@@ -38,4 +35,4 @@
 //
 //        return new ResponseEntity<>(responseMapper, HttpStatus.OK);
 //    }
-//}
+}
--- system-design/spring-boot/petonix/src/main/java/in/pets/petonix/repository/AddressRepository.java
+++ system-design/spring-boot/petonix/src/main/java/in/pets/petonix/repository/AddressRepository.java
@@ -1,10 +1,9 @@
-//package in.pets.petonix.repository;
-//
-//import in.pets.petonix.model.ums.Address;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface AddressRepository  extends MongoRepository<Address, Long>, QuerydslPredicateExecutor<Address> {
-//}
+package in.pets.petonix.repository;
+
+import in.pets.petonix.model.ums.Address;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository  extends MongoRepository<Address, String> {
+}
